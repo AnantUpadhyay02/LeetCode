750b7bbf8f44a71390f973064147312f80250adf
Anant Upadhyay <113023270+AnantUpadhyay02@users.noreply.github.com>
2023-09-08 13:49:44 +0530

    Time: 12 ms (79.89%) | Memory: 17 MB (36.02%) - LeetSync

---
+++ 136-single-number/single-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& arr) {
+        int xor1 = 0;
+	for(int i =0;i<arr.size();i++){
+		xor1 = xor1^arr[i];
+	}
+	return xor1;
+        
+    }
+};
