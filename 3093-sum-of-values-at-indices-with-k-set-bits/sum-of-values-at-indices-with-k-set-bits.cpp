d2fa0d39ced38ef33182b7afadcd4b9a6650a31b
Anant Upadhyay <113023270+AnantUpadhyay02@users.noreply.github.com>
2023-09-29 22:27:41 +0530

    Time: 7 ms (82.32%) | Memory: 21.8 MB (92.23%) - LeetSync

---
+++ 3093-sum-of-values-at-indices-with-k-set-bits/sum-of-values-at-indices-with-k-set-bits.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int sumIndicesWithKSetBits(vector<int>& nums, int k) {
+        int sum = 0;
+        for(int indx = 0; indx < nums.size(); indx++){
+            if(__builtin_popcount(indx) == k){
+                sum += nums[indx];
+            }
+        }
+        return sum;
+    }
+};
